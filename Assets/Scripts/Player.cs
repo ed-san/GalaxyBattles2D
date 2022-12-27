b3c212b3758310288eaefc3580758272a4d4a44c
Eddie Sanchez <eddielusanchez@gmail.com>
2022-12-26 21:20:23 -0800

    Refactored movement logic into its own function.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed = 8.0f;
 
 
     // Start is called before the first frame update
@@ -20,42 +20,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        calculateMovement();
+    }
+
+    void calculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(Time.deltaTime * _speed * direction);
         
-        /* If player position on the y is greater than 0
-         * y position = 0
-         * else if position on the y is less than -3.8f
-         * y position = -3.8f
-         */
+       // The code below restricts Y-Axis movement between the ranges of -3.8f and 3.0f creating a vertical boundary for player.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 3.0f),0);
         
+        /*
+           This if block allows the player to wrap around the screen
+           when going off-screen on the left or right side along the X-Axis.
+        */
         
-        if (transform.position.y >= 3.0f)
-        {
-            transform.position = new Vector3(transform.position.x, 3.0f, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
-        
-        /* if player on the x > 11
-         * x position = -11
-         * else if player on the x is less than -11
-         * x position = 11
-         */
-        //14.2 & -14.2 are the edges of the screen
-
-        if (transform.position.x >= 14.2f)
+        if (transform.position.x >= 11.3f)
         {
             float xPosOffScreenRight = transform.position.x * -1;
             float yPosOffScreenRight = transform.position.y * 1;
             
             transform.position = new Vector3(xPosOffScreenRight, yPosOffScreenRight, 0);
         }
-        else if (transform.position.x <= -14.2f)
+        else if (transform.position.x <= -11.3f)
         {
             float xPosOffScreenLeft = transform.position.x * -1;
             float yPosOffScreenLeft = transform.position.y * 1;
@@ -63,4 +53,5 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(xPosOffScreenLeft,  yPosOffScreenLeft, 0);
         }
     }
+    
 }
