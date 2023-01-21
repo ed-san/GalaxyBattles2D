8dab66a6b8ea4ed6b067bbeeef6c567985df899c
Eddie <eddielusanchez@gmail.com>
2023-01-21 10:33:31 -0800

    Added gameover state text and flicker effect.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,12 +13,14 @@ public class UIManager : MonoBehaviour
     private Image _livesImage;
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private TMP_Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
-        
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public string UpdateScore(int score)
@@ -28,9 +30,24 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int currentLives)
     {
-        //display image sprite
-        //give it a new image based on currentLives index value
         _livesImage.sprite = _liveSprites[currentLives];
 
+        if (currentLives == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlickerRoutine());
+        }
     }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(.05f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(.05f);
+        }
+    }
+
 }
